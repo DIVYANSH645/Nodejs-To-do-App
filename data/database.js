6d494ebac0675d042b930cbaa5b5c16bd64c09c4
Divyansh Sharma <82891741+DIVYANSH645@users.noreply.github.com>
2024-02-23 11:44:33 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017", {
+    .connect(process.env.MONGO_URI, {
       dbName: "backendapi",
     })
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
